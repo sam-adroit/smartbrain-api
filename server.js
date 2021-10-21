7c2a53e6e43bdc32b20145a5b8856b0138c0f042
sam-adroit <samuelbankole779@gmail.com>
2021-10-21 12:06:07 +0100

    C:/Program Files/Git/ route send data

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json())
 app.use(cors())
 
 
-app.get('/', (req,res) => {res.send(db.users);})
+app.get('/', (req,res) => {res.send('It is working');})
 app.post('/signin', signin.handleSignin(db, bcrypt));
 app.post('/register', (req,res) => {register.handleRegister(req,res, db, bcrypt )})
 app.get('/profile/:id', (req,res) => profile.handleProfileGet(req,res, db))
