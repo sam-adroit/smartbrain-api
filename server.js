34bcfae719d84e481dcc79c82873aac14a57bb73
sam-adroit <samuelbankole779@gmail.com>
2021-10-21 08:10:59 +0100

    Backend code

---
+++ server.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const proile = require('./controllers/profile');
+const image = require('./controllers/imageurl');
+
+
+const db = require('knex')({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'postgres',
+      password : 'Success123!',
+      database : 'smartbrain'
+    }
+});
+
+const app = express();
+
+app.use(bodyParser.json())
+app.use(cors())
+
+
+app.get('/', (req,res) => {res.send(db.users);})
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', (req,res) => {register.handleRegister(req,res, db, bcrypt )})
+app.get('/profile/:id', (req,res) => profile.handleProfileGet(req,res, db))
+app.put('/image', (req, res) => image.handleImgUrl(req,res, db));
+app.post('/imageurl', (req, res) => image.handleApiCall(req,res ));
+
+
+
+app.listen(3000, () => {
+    console.log('App is running on port 3000');
+})
